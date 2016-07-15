3f160fc1b9ce2a36d3996f91c4d59ab0f0d9384b
Glendon Cheney <glendon.cheney@gmail.com>
2016-07-15 18:38:54 -0500

    Create database connection

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // ------------------- REQUIRES ------------------------ //
 var express         = require('express'),
     app             = express(),
-    morgan          = require('morgan')
-    bodyParser      = require('body-parser')
+    morgan          = require('morgan'),
+    bodyParser      = require('body-parser'),
+    mongoose        = require('mongoose')
 
 // ------------------- INITIAL APP SETTINGS ------------------------ //
 var PORT = process.env.PORT || 3000;
@@ -10,6 +11,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('combined'));
+
+// ------------------- REQUIRE ROUTES ------------------------ //
+var homeRoutes = require('./routes/home');
+
+// ---------- DATABASE CONFIGURATION ----------- //
+//mongodb://<dbuser>:<dbpassword>@ds035014.mongolab.com:35014/db_name
+var DB_URL = process.env.DATABASE_URL || 'mongodb://localhost/coder_tutorials';
+mongoose.connect(DB_URL);
 
 
 // ------------------- APP LISTEN ------------------------ //
