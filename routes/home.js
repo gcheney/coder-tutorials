88ac7ee66440a32e97035c30e11b4764195e2373
Glendon Cheney <glendon.cheney@gmail.com>
2016-07-16 12:11:11 -0500

    Update home route query to limit returned records

--- routes/home.js
+++ routes/home.js
@@ -4,17 +4,20 @@ var Tutorial = require('../models/tutorial');
 
 // GET: /
 router.get('/', function(req, res){
-    //Get all tutorials from the db
-    Tutorial.find({}, function(err, tutorials){
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('home/index', { 
-                title: 'Home',
-                tutorials: tutorials
+    //Get all tutorials from the db sorted by desc date
+    Tutorial.find({})
+            .sort({'createdOn': 'desc'})
+            .limit(5)
+            .exec(function(err, tutorials) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.render('home/index', { 
+                        title: 'Home',
+                        tutorials: tutorials
+                    });
+                }
             });
-        }
-    });
 });
 
 module.exports = router;
