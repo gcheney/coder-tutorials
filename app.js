ebbff6a36c1047df9c54d75e5f1664cd2b3cefdd
Glendon Cheney <glendon.cheney@gmail.com>
2016-07-15 19:09:19 -0500

    Require additional routes

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var express         = require('express'),
     seedDB          = require('./seeds')
     
     
+    
 // ------------------- INITIAL APP SETTINGS ------------------------ //
 var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
@@ -23,19 +24,28 @@ app.use(methodOverride('_method'));
 app.use(morgan('combined'));
 app.use(flash());
 
+
 // ------------------- REQUIRE ROUTES ------------------------ //
-var homeRoutes = require('./routes/home');
+var homeRoutes          = require('./routes/home'),
+    tutorialRoutes      = require('./routes/tutorials'),
+    reviewRoutes        = require('./routes/reviews'),
+    accountRoutes       = require('./routes/account')
+
 
 // ---------- DATABASE CONFIGURATION ----------- //
 //mongodb://<dbuser>:<dbpassword>@ds035014.mongolab.com:35014/db_name
 var DB_URL = process.env.DATABASE_URL || 'mongodb://localhost/coder_tutorials';
 mongoose.connect(DB_URL);
 
+
 // ------------------- USE ROUTES ------------------------ //
+app.use('/tutorials', tutorialRoutes);
+app.use('/tutorials/:id/reviews', reviewRoutes);
+app.use('/account', accountRoutes);
 app.use(homeRoutes);
 
-// ------------------- APP LISTEN ------------------------ //
 
+// ------------------- APP LISTEN ------------------------ //
 app.listen(PORT, function(req, res){
     console.log('Server is listening on port ' + PORT); 
     console.log('http://127.0.0.1:' + PORT);
