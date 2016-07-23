f4a8e7ada2d33ef4630393c3fdc2cdc85d593d27
Glendon Cheney <glendon.cheney@gmail.com>
2016-07-23 15:33:19 -0500

    Add animation on removal of flash message

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
     
-    
-    
+    //Fades out the flash message and moves up remaining content
+    $(".close").on("click", function() {
+        $( "#flash-container").fadeTo("slow", 0.00, function(){ 
+             $(this).slideUp("fast", function() { 
+                 $(this).remove(); 
+             });
+        });
+    });
+
 });
