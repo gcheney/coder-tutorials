232ae44551e2d7317aeedd55800550a450e1a2a1
Glendon Cheney <glendon.cheney@gmail.com>
2016-07-24 11:07:01 -0500

    Add 404 error support to app

--- app.js
+++ app.js
@@ -58,6 +58,26 @@ accountController(app);
 tutorialController(app);
 reviewController(app);
 
+// ------------------- 404 HANDLER ------------------------ //
+app.use(function(req, res, next){
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('404', { url: req.url });
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+});
+
 
 // ------------------- APP LISTEN ------------------------ //
 app.listen(PORT, function(req, res){
