b642bfae2c86bc9370371a2d3d52d856496a1326
Glendon Cheney <glendon.cheney@gmail.com>
2016-07-28 15:53:34 -0500

    Add connection process for heroku and nodemon restart

--- data/db.js
+++ data/db.js
@@ -1,17 +1,50 @@
 var mongoose = require('mongoose');  
+var seedDb = require('./seed');
 
 //mongodb://<dbuser>:<dbpassword>@ds035014.mongolab.com:35014/db_name
-var DB_URI = process.env.MONGOLAB_URI || 'mongodb://localhost/coder_tutorials';
-mongoose.connect(DB_URI);
+var dbURI = 'mongodb://localhost/coder_tutorials';
+if (process.env.NODE_ENV === 'production') {
+    dbURI = process.env.MONGOLAB_URI;
+}
+mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', function () {
-    console.log('Mongoose connected to ' + DB_URI);
+    console.log('Mongoose connected to ' + dbURI);
+    seedDb(); 
 });
 
-mongoose.connection.on('error',function (err) {
+mongoose.connection.on('error', function (err) {
     console.log('Mongoose connection error: ' + err);
 });
 
 mongoose.connection.on('disconnected', function () {
     console.log('Mongoose disconnected');
+});
+
+var gracefulShutdown = function (msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+
+// For nodemon restarts
+process.once('SIGUSR2', function () {
+    gracefulShutdown('nodemon restart', function () {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+// For app termination
+process.on('SIGINT', function() {
+    gracefulShutdown('app termination', function () {
+        process.exit(0);
+    });
+});
+
+// For Heroku app termination
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app shutdown', function () {
+        process.exit(0);
+    });
 });
