787a93c5b4514f2e3ba80583a8e85c581bacf6db
Glendon Cheney <glendon.cheney@gmail.com>
2016-08-05 23:07:35 -0500

    Add flash timeout and are-you-sure plugin

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,32 +1,45 @@
 $(document).ready(function() {
     
-    //Fades out the flash message and moves up remaining content
-    $(".close").on("click", function() {
-        $( "#flash-container").fadeTo("slow", 0.00, function(){ 
-             $(this).slideUp("fast", function() { 
+    // Enable on all forms
+    $('form').areYouSure();    
+    
+    // gracefully remove flash message container
+    function flashFadeout() {
+        $('#flash-container').fadeTo('fast', 0.00, function() { 
+             $(this).slideUp('slow', function() { 
                  $(this).remove(); 
              });
         });
+    }
+    
+    // fade out flash message
+    setTimeout(function() {
+          flashFadeout();
+    }, 5000);
+    
+    // user removes flash message
+    $('.close').on('click', function(){
+        flashFadeout();
     });
     
     // show popup modal for tutorial delete
-    $("#tutorial-delete").on("click", function(e){
+    $('#tutorial-delete').on('click', function(e) {
         e.preventDefault();
              
-        $("#btn-ok").on("click", function() {
-            $("#confirm-delete").fadeOut("fast", function() {
-                 $("#delete-form").trigger("submit"); // submit the form
+        $('#btn-ok').on('click', function() {
+            $('#confirm-delete').fadeOut('fast', function() {
+                 $('#delete-form').trigger('submit'); // submit the form
              });
         });        
     });
     
     // show confirm box for review delete
-    $(".btn-delete").on("click", function(){
-        confirm("Are you sure you want to delete this review?");
+    $('.btn-delete').on('click', function() {
+        confirm('Are you sure you want to delete this review?');
     });
     
     // sign up form validation rules
-    $("#signup-form").validate({
+    $('#signup-form').validate({
         rules: {
             username: {
                 required: true,
@@ -40,13 +53,13 @@ $(document).ready(function() {
         },
         messages: {
             username: {
-                required: "Please provide a username",
-                maxlength: "Your usernme cannot exceed 26 characters"
+                required: 'Please provide a username',
+                maxlength: 'Your usernme cannot exceed 26 characters'
             },
             password: {
-                required: "Please provide a password",
-                minlength: "Your password must be at least 6 characters long",
-                maxlength: "Your password cannot exceed 26 characters"
+                required: 'Please provide a password',
+                minlength: 'Your password must be at least 6 characters long',
+                maxlength: 'Your password cannot exceed 26 characters'
             }
         },
         submitHandler: function(form) {
@@ -55,7 +68,7 @@ $(document).ready(function() {
     });
     
     // review form validation rules
-    $("#review-form").validate({
+    $('#review-form').validate({
         rules: {
             content: {
                 required: true,
@@ -64,8 +77,8 @@ $(document).ready(function() {
         },
         messages: {
             content: {
-                required: "Please provide a full review",
-                maxlength: "Please keep your review under 1000 characters"
+                required: 'Please provide a full review',
+                maxlength: 'Please keep your review under 1000 characters'
             }
         },
         submitHandler: function(form) {
@@ -73,7 +86,7 @@ $(document).ready(function() {
         }
     });
     
-    $("#details-form").validate({
+    $('#details-form').validate({
         rules: {
             details: {
                 required: true,
@@ -82,8 +95,8 @@ $(document).ready(function() {
         },
         messages: {
             details: {
-                required: "Please provide some details about yourself",
-                maxlength: "Please keep your details under 1500 characters"
+                required: 'Please provide some details about yourself',
+                maxlength: 'Please keep your details under 1500 characters'
             }
         },
         submitHandler: function(form) {
@@ -91,7 +104,7 @@ $(document).ready(function() {
         }
     });
     
-    $("#new-password-form").validate({
+    $('#new-password-form').validate({
         rules: {
             newPassword: {
                 required: true,
@@ -106,14 +119,14 @@ $(document).ready(function() {
         },
         messages: {
             newPassword: {
-                required: "Please provide a new password",
-                maxlength: "Your password cannot exceed 26 characters",
-                minlength: "Your password must be at least 6 characters long"
+                required: 'Please provide a new password',
+                maxlength: 'Your password cannot exceed 26 characters',
+                minlength: 'Your password must be at least 6 characters long'
             },
             confirmPassword: {
-                required: "Please confirm your new password",
-                maxlength: "Your password cannot exceed 26 characters",
-                minlength: "Your password must be at least 6 characters long"
+                required: 'Please confirm your new password',
+                maxlength: 'Your password cannot exceed 26 characters',
+                minlength: 'Your password must be at least 6 characters long'
             }
         },
         submitHandler: function(form) {
@@ -121,7 +134,7 @@ $(document).ready(function() {
         }
     });
     
-    $("#tutorial-form").validate({
+    $('#tutorial-form').validate({
         rules: {
             title: {
                 required: true,
@@ -131,19 +144,19 @@ $(document).ready(function() {
                 required: true,
                 maxlength: 250
             },
-            content: "required"
+            content: 'required'
         },
         messages: {
             title: {
-                required: "Please provide a title for the tutorial",
-                maxlength: "Your tutorials title cannot exceed 100 characters"
+                required: 'Please provide a title for the tutorial',
+                maxlength: 'Your tutorials title cannot exceed 100 characters'
             },
             description: {
-                required: "Please include a brief (250 character) tutorial description",
-                maxlength: "Your description cannot exceed 250 characters"
+                required: 'Please include a brief (250 character) tutorial description',
+                maxlength: 'Your description cannot exceed 250 characters'
             },
             content: {
-                required: "Don't forget to write your tutorial!"
+                required: 'Don\'t forget to write your tutorial!'
             }
         },
         submitHandler: function(form) {
