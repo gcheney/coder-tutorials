aa6a6e9bcbb888e8066eee81bc8aefc1a97226cc
Glendon Cheney <glendon.cheney@gmail.com>
2016-08-20 12:45:14 -0500

    Setup github auth file and strategy

---
+++ src/auth/github.js
@@ -0,0 +1,50 @@
+var passport = require('passport');
+var GitHubStrategy = require('passport-github2').Strategy;
+
+var User = require('../models/user');
+var initUser = require('./init');
+
+// not valid - used to run app without github oAuth errors
+var strategy = {
+    clientID: '4c34e57b18ed6d68091a',
+    clientSecret: '0c9663c587bfe44d625ce7aef496ca1ff3e58198',
+    callbackURL: 'http://127.0.0.1:3000/account/github/auth'
+};
+
+if (process.env.NODE_ENV === 'production') {
+    strategy = {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: process.env.GITHUB_CALLBACK_URL
+    };
+}
+
+var githubCallback = function(accessToken, refreshToken, profile, done) {
+
+    var searchQuery = {
+        username: profile.displayName
+    };
+
+    var updates = { 
+        username: profile.displayName 
+    };
+
+    var options = {
+        upsert: true
+    };
+
+    User.findOneAndUpdate(searchQuery, updates, options, function(err, user) {
+        if(err) {
+            return done(err);
+        } else {
+            return done(null, user);
+        }
+    });
+};
+
+passport.use(new GitHubStrategy(strategy, githubCallback));
+
+// serialize user into the session
+initUser();
+
+module.exports = passport;
