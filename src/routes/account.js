d25987fd0e7b7b38287d551f5ae43469b5c10165
Glendon Cheney <glendon.cheney@gmail.com>
2016-08-20 12:47:17 -0500

    Add routes for github auth and callback

--- src/routes/account.js
+++ src/routes/account.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var passportGithub = require('../auth/github');
 var authMiddleware = require('../middleware/auth');
 var accountController = require('../controllers/account');
 
@@ -9,14 +10,28 @@ router.get('/signup', accountController.signup);
 
 router.post('/signup', accountController.doSignup);
 
+router.get('/github', passportGithub.authenticate('github', { scope: [ 'user:email' ] }));
+
+// if github auth fails to authenticate 
+var githubFailureResult = { 
+    failureRedirect: '/account/login', 
+    failureFlash: 'Unable to authenticate via Github - please try again.' 
+};
+router.get('/github/auth', passportGithub.authenticate('github', githubFailureResult), 
+            accountController.doLogin);
+
+
 router.get('/manage', authMiddleware.isAuthenticated, 
            accountController.manage);
+
 
 router.post('/manage/password', authMiddleware.isAuthenticated, 
             accountController.updatePassword);
 
+
 router.post('/manage/details', authMiddleware.isAuthenticated, 
             accountController.updateDetails);
+
 
 router.get('/login', accountController.login);
 
@@ -24,10 +39,12 @@ router.get('/login', accountController.login);
 var failureResult = { 
     failureRedirect: '/account/login', 
     failureFlash: 'Invalid username or password - please try again.' 
-}
+};
 router.post('/login', passport.authenticate('local', failureResult), 
             accountController.doLogin);
 
+
 router.get('/logout', accountController.logout);
+
 
 module.exports = router;
